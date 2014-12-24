bc513e2401b5b2362221ff0ddcf36ace392e0d54
Clint <clintp1@gmail.com>
2014-12-24 16:32:17 +0000

    initial commit and added test for menu class, test green so far

---
+++ spec/menu_spec.rb
@@ -0,0 +1,28 @@
+require 'Menu'
+
+describe Menu do
+  
+  it 'should display food selection' do
+
+  end
+
+  it 'should display food prices' do
+
+  end
+
+  it 'should take order quantities per item' do
+
+  end
+
+  it 'should take a number equal to the total amount of order' do
+
+  end
+
+  it 'raise an error if the number given is not equal to the total amount' do
+
+  end
+
+
+
+
+end
